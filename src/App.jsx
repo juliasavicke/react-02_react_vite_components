74a46c0e8f76bb1f08697e16e21212ca4bbc7885
juliasavicke <juliasavicke@gmail.com>
2022-11-22 09:45:36 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+function SectionTitle(props) {
+  console.log('props ===', props);
+  return <h2 className='sectionTitle'>{props.title}</h2>;
+}
 
+function Icon(props) {
+  return <i className={`fa ${props.iconName}`} aria-hidden='true'></i>;
+}
+
+function Grid() {
+  return (
+    <div className='grid'>
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+    </div>
+  );
+}
+function ServiceCard() {
+  return (
+    <div className='card'>
+      <Icon iconName='fa-address-book' />
+      <h3 className='card__title'>title</h3>
+      <p className='card__text'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi,
+        molestiae! Est soluta sint amet illo delectus nisi sit. Consequatur,
+        illo.
+      </p>
+      <a href='#' className='card__link'>
+        Learn more
+      </a>
+    </div>
+  );
+}
+
+function App() {
   return (
     <div className='App container'>
-      <h1>Hello Vite React</h1>
+      <SectionTitle title='Musu paslaugos' size='large' />
+      <Grid />
     </div>
   );
 }
