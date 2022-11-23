afceb15cbc5bb601e906f3be3fc8b8a45433ce05
juliasavicke <juliasavicke@gmail.com>
2022-11-23 11:21:50 +0200

    map

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -21,21 +21,9 @@ const cardsArr = [
 function Grid() {
   return (
     <div className='grid'>
-      <ServiceCard
-        title={cardsArr[0].title}
-        icon={cardsArr[0].icon}
-        link={cardsArr[0].link}
-      />
-      <ServiceCard
-        title={cardsArr[1].title}
-        icon={cardsArr[1].icon}
-        link={cardsArr[1].link}
-      />
-      <ServiceCard
-        title={cardsArr[2].title}
-        icon={cardsArr[2].icon}
-        link={cardsArr[2].link}
-      />
+      {cardsArr.map((cObj) => (
+        <ServiceCard title={cObj.title} icon={cObj.icon} link={cObj.link} />
+      ))}
     </div>
   );
 }
